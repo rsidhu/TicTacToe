972da1e0f42d084ffc3303d98415fb3425656b96
rsidhu <rahul@live-area.com>
2015-05-18 17:39:18 -0700

    Cleaned up some loose files (unused js files specifically)

--- js/controllers.js
+++ js/controllers.js
@@ -10,48 +10,35 @@ angular
     	self.p2Local = false;
     	console.log("Controller loaded.")
 
+    	// Pulls board from firebase
     	self.boxes = (function(){
         	var ref = new Firebase("https://tictactopgun.firebaseio.com/boxes");
 			var moves = $firebaseArray(ref);
 			return moves;
         })();
 
+        // Pulls counters from firebase
         self.counters = (function(){
         	var ref = new Firebase("https://tictactopgun.firebaseio.com/counters");
         	var counter = $firebaseArray(ref);
         	return counter;
         })();
 
+        //Pulls chat messages from firebase
         self.chats = (function(){
             var ref = new Firebase("https://tictactopgun.firebaseio.com/chats");
             var chats = $firebaseArray(ref);
             return chats;
         })();
 
+        //Adds chat messages
         self.addChat = function(){
             console.log("adding chat");
             self.chats.$add({message: self.text});
             self.text = null;
         }
 
-        // self.players = (function(){
-       	// 	var ref = new Firebase("https://tictactopgun.firebaseio.com/players")
-       	// 	var players = $firebaseArray(ref);
-       	// 	return players;
-       	// })();
-
-       	// self.p1Logger = function(){
-       	// 	console.log("Inputting P1");
-       	// 	self.players[0].$value = self.p1namer;
-       	// 	self.players.$save(self.players[0]);
-       	// }
-
-       	// self.p2Logger = function(){
-       	// 	console.log("Inputting P2");
-       	// 	self.players[1].$value = self.p2namer;
-       	// 	self.players.$save(self.players[1]);
-       	// }
-
+        //Selects an X or an O
        	self.playerMove = function($index){
        		console.log("Playing");
        		if (self.p1Local == false && self.p2Local == false && self.counters[2].$value == 0){
@@ -85,6 +72,7 @@ angular
        		self.counters.$save(self.counters[3]);
        	}
 
+       	//Determines who wins (or if there's a tie)
        	self.winLogic = function(){
         	if (
 	        		self.boxes[0].moveX == true && self.boxes[1].moveX == true && self.boxes[2].moveX == true ||
@@ -97,7 +85,8 @@ angular
 	        		self.boxes[2].moveX == true && self.boxes[4].moveX == true && self.boxes[6].moveX == true 
 	        	){
 	        		self.counters[0].$value++;
-	        		self.reset();
+	        		//self.reset();
+	        		alert("Goose wins");
         		} else if (
 	        		self.boxes[0].moveY == true && self.boxes[1].moveY == true && self.boxes[2].moveY == true ||
 	        		self.boxes[3].moveY == true && self.boxes[4].moveY == true && self.boxes[5].moveY == true ||
@@ -110,12 +99,14 @@ angular
 		        ){
 	        		self.counters[1].$value++;
 	        		self.reset();
+	        		alert("Maverick wins!");
         		} else if (self.counters[3].$value >= 9){
         			alert("It's a tie douchebag!");
         			self.reset();
         		}
         }
 
+        //Resets everything
         self.reset = function(){
         	console.log("Testing")
         	for (var i = 0; i < 9; i++) {
@@ -125,13 +116,10 @@ angular
             self.counters[1].$value = 0;
             self.counters[2].$value = 0;
             self.counters[3].$value = 0;
-        	// self.players[0].$value = "";
-        	// self.players[1].$value = "";
-        	// self.players.$save(self.players[0]);
-        	// self.players.$save(self.players[1]);
         	self.boxes.$save(self.boxes[i]);
         	self.counters.$save(self.counters[i]);
         	}
     	}
 
+    	
     }
